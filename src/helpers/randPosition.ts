676476aed9540c1a679ca3458e50fff108a96ef8
Alexander Larin <doremilarin@gmail.com>
2023-02-05 16:55:36 +0300

    view-brunch init

--- src/helpers/randPosition.ts
+++
@@ -1,5 +0,0 @@
-export const randPosition = (cellSize: number, cellsQty: number): number => {
-    let randInt = Math.floor(Math.random() * cellsQty);
-    let randStep = cellSize * randInt;
-    return randStep;
-}
